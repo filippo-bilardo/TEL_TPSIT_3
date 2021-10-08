964d1fdf4ac88f818af625b541cdaf3a2a498584
Filippo <filippo.bilardo@tiscali.it>
2021-10-08 20:08:45 +0200

    Update main.c
    External Interrupt example. Toggle red led light when PA3 button is pressed.

--- 003/main.c
+++ 003/main.c
@@ -32,7 +32,7 @@
 void EXTI3_IRQHandler(void) {  
   if ((EXTI->PR1 & EXTI_PR1_PIF3) != 0) {
     // Toggle LED
-		GPIOB->ODR ^= GPIO_ODR_ODR_2;
+    GPIOB->ODR ^= GPIO_ODR_ODR_2;
     // Cleared flag by writing 1
     EXTI->PR1 |= EXTI_PR1_PIF3;
   }
@@ -40,39 +40,39 @@ void EXTI3_IRQHandler(void) {
 
 int main(void)
 {
-	/* ********************************************************************************************
-	* Led red Init
-	* LD4 Red = PB2
-	******************************************************************************************** */
-	///1. Enable the peripheral clock of GPIO Port	
-	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN | RCC_AHB2ENR_GPIOEEN;
-	///2. GPIO Mode: Input(00), Output(01), AlterFunc(10), Analog(11, reset)
-	GPIOB->MODER &= ~(3U<<(2*2));  
-	GPIOB->MODER |= 1U<<(2*2);      //  Output(01)
-	///3. GPIO Speed: Low speed (00), Medium speed (01), Fast speed (10), High speed (11)
-	GPIOB->OSPEEDR &= ~(3U<<(2*2));	// Azzero i due bit di PB2
-	GPIOB->OSPEEDR |=   3U<<(2*2);  // Imposto a uno i due bit di PB2 -> High speed
-	///4. GPIO Output Type: Output push-pull (0, reset), Output open drain (1) 
-	GPIOB->OTYPER &= ~(1U<<2);      // Push-pull
-	///5. GPIO Push-Pull: No pull-up, pull-down (00), Pull-up (01), Pull-down (10), Reserved (11)
-	GPIOB->PUPDR   &= ~(3U<<(2*2)); // No pull-up, no pull-down
+  /* ********************************************************************************************
+  * Led red Init
+  * LD4 Red = PB2
+  ******************************************************************************************** */
+  ///1. Enable the peripheral clock of GPIO Port  
+  RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN | RCC_AHB2ENR_GPIOEEN;
+  ///2. GPIO Mode: Input(00), Output(01), AlterFunc(10), Analog(11, reset)
+  GPIOB->MODER &= ~(3U<<(2*2));  
+  GPIOB->MODER |= 1U<<(2*2);      //  Output(01)
+  ///3. GPIO Speed: Low speed (00), Medium speed (01), Fast speed (10), High speed (11)
+  GPIOB->OSPEEDR &= ~(3U<<(2*2)); // Azzero i due bit di PB2
+  GPIOB->OSPEEDR |=   3U<<(2*2);  // Imposto a uno i due bit di PB2 -> High speed
+  ///4. GPIO Output Type: Output push-pull (0, reset), Output open drain (1) 
+  GPIOB->OTYPER &= ~(1U<<2);      // Push-pull
+  ///5. GPIO Push-Pull: No pull-up, pull-down (00), Pull-up (01), Pull-down (10), Reserved (11)
+  GPIOB->PUPDR   &= ~(3U<<(2*2)); // No pull-up, no pull-down
 
-	/* ********************************************************************************************
-	* SW3 Init (PA3)
-	* Joystick (MT-008A): 
-	*   Right = PA2        Up   = PA3         Center = PA0
-	*   Left  = PA1        Down = PA5
-	* LD4 Red = PB2
-	******************************************************************************************** */
-  /// 1. Enable the peripheral clock of GPIO Port	
-  RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;	
+  /* ********************************************************************************************
+  * SW3 Init (PA3)
+  * Joystick (MT-008A): 
+  *   Right = PA2        Up   = PA3         Center = PA0
+  *   Left  = PA1        Down = PA5
+  * LD4 Red = PB2
+  ******************************************************************************************** */
+  /// 1. Enable the peripheral clock of GPIO Port  
+  RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;  
   /// 2. GPIO Mode: Input(00), Output(01), AlterFunc(10), Analog(11, reset)
   GPIOA->MODER &= ~3U << 6;
   /// 3. GPIO Push-Pull: No pull-up, pull-down (00), Pull-up (01), Pull-down (10), Reserved (11)
   GPIOA->PUPDR &= ~3U << 6;
   GPIOA->PUPDR |= 2U << 6;    // Pull down
-	
-	/// 4. Enable Interrupt [opional]
+  
+  /// 4. Enable Interrupt [opional]
   NVIC_EnableIRQ(EXTI3_IRQn); 
   // Connect External Line to the GPI
   RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
@@ -82,7 +82,7 @@ int main(void)
   EXTI->IMR1  |= EXTI_IMR1_IM3;     
   // Rising trigger selection: 0 = trigger disabled, 1 = trigger enabled
   EXTI->RTSR1 |= EXTI_RTSR1_RT3;  
-	
-	// Main loop. Waiting for the interurpt.
-	for(;;) {;}
+  
+  // Main loop. Waiting for the interurpt.
+  for(;;) {;}
 }
