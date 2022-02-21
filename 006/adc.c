a2ea1b2137c677d88e3bff7bf3774f906d8c8252
Filippo <filippo.bilardo@tiscali.it>
2022-02-21 19:03:15 +0100

    Update adc.c

--- 006/adc.c
+++ 006/adc.c
@@ -1,36 +1,69 @@
 #include "adc.h"
 #include "stm32l4xx.h"
 
-void adc1Init() {
-	int TADCVREG_STUP=SystemCoreClock/1000000*20;  //20 us
-	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;  //configurazione PA2 input mode
-	GPIOA->MODER |= GPIO_MODER_MODE2_Msk;
-	GPIOA->ASCR |= GPIO_ASCR_EN_2; //collegamento PA2 con ADC1
-	RCC->AHB2ENR |= RCC_AHB2ENR_ADCEN;//abilitazione clock ADC1
-	ADC1->CR &= ~ ADC_CR_ADEN;
+// Analog Inputs: 
+// PA2 (ADC12_IN7)
+// These pins are not used: PA0 (ADC12_IN5), PA1 (ADC12_IN6), PA3 (ADC12_IN8)
 
-	SYSCFG->CFGR1 |= SYSCFG_CFGR1_BOOSTEN;
-	ADC123_COMMON->CCR &= ~ ADC_CCR_VREFEN;
-	ADC123_COMMON->CCR &= ~ ADC_CCR_PRESC;
-	ADC123_COMMON->CCR |= ADC_CCR_CKMODE_0;
-	ADC123_COMMON->CCR &= ~ ADC_CCR_DUAL;
-	//adc1Wakeup();
-	ADC1->CR &= ~ADC_CR_DEEPPWD; //uscita Deep-power-down mode
-	ADC1->CR |= ADC_CR_ADVREGEN; //abilitazione voltage regulator
+//******************************************************************************************
+// STM32L4x6xx Errata sheet
+// When the delay between two consecutive ADC conversions is higher than 1 ms the result of 
+// the second conversion might be incorrect. The same issue occurs when the delay between the 
+// calibration and the first conversion is higher than 1 ms.
+// Workaround
+// When the delay between two ADC conversions is higher than the above limit, perform two ADC 
+// consecutive conversions in single, scan or continuous mode: the first is a dummy conversion 
+// of any ADC channel. This conversion should not be taken into account by the application.
+
+
+void adc1Init() {
+	
+	int TADCVREG_STUP=SystemCoreClock/1000000*20;  //20 us
+	
+	//ADC Pin Initialization
+	// Enable the clock of GPIO Port A
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;  //configurazione PA2 input mode
+	// GPIO Speed: Low speed (00), Medium speed (01), Fast speed (10), High speed (11)
+	// GPIO Output Type: Output push-pull (0, reset), Output open drain (1)	
+	// GPIO Mode: Input(00), Output(01), AlterFunc(10), Analog(11, reset)
+	//GPIOA->MODER |= GPIO_MODER_MODE2_Msk;
+	//GPIOA->MODER |=  3U<<(2*1);  // PA1 (ADC12_IN6), Mode 11 = Analog
+	GPIOA->MODER |=  3U<<(2*2);  // PA2 (ADC12_IN7), Mode 11 = Analog
+	// GPIO Push-Pull: No pull-up, pull-down (00), Pull-up (01), Pull-down (10), Reserved (11)
+	GPIOA->PUPDR &= ~(3U<<(2*2)); //PA2 No pull-up, no pull-down
+	
+	// GPIO port analog switch control register (ASCR)
+	// 0: Disconnect analog switch to the ADC input (reset state)
+	// 1: Connect analog switch to the ADC input
+	GPIOA->ASCR |= GPIO_ASCR_EN_1;  //collegamento PA1 con ADC1
+	GPIOA->ASCR |= GPIO_ASCR_EN_2;  //collegamento PA2 con ADC1
+	RCC->AHB2ENR |= RCC_AHB2ENR_ADCEN;  //abilitazione clock ADC1
+	ADC1->CR &= ~ ADC_CR_ADEN;  //disabilitazione ADC1
+
+	SYSCFG->CFGR1 |= SYSCFG_CFGR1_BOOSTEN;  //enable I/O analog switches voltage booster
+	ADC123_COMMON->CCR &= ~ ADC_CCR_VREFEN; //enable the conversion of internal channels
+	ADC123_COMMON->CCR &= ~ ADC_CCR_PRESC;  //Configure the ADC prescaler to select the frequency of the clock to the ADC (set clock not divided)
+	ADC123_COMMON->CCR |= ADC_CCR_CKMODE_0; //set the ADC clock mode as synchronous clock mode (HCLK/1)
+	ADC123_COMMON->CCR &= ~ ADC_CCR_DUAL;   //Configure all ADCs as independent
+	//By default, the ADC is in deep-power-down mode where its supply is internally switched off to reduce the leakage currents.
+	//Therefore, software needs to wait up ADC
+	//adc1Wakeup(); 
+	ADC1->CR &= ~ADC_CR_DEEPPWD;  //uscita Deep-power-down mode
+	ADC1->CR |= ADC_CR_ADVREGEN;  //abilitazione voltage regulator
 	while(TADCVREG_STUP) {        //attesa abilitazione regulator
 		TADCVREG_STUP--;
 	}
 
 	//calibrazione
-	ADC1->CR &= ~ ADC_CR_ADCALDIF;//calibrazione modalita' singola
-	ADC1->CR |= ADC_CR_ADCAL;     //inizio calibrazione
-	while(ADC1->CR&ADC_CR_ADCAL); //attesa calibrazione
+	ADC1->CR &= ~ ADC_CR_ADCALDIF; //calibrazione modalita' singola
+	ADC1->CR |= ADC_CR_ADCAL;      //inizio calibrazione
+	while(ADC1->CR&ADC_CR_ADCAL);  //attesa calibrazione
 	
-	ADC1->CFGR &= ~ ADC_CFGR_RES;
-	ADC1->CFGR &= ~  ADC_CFGR_ALIGN;
-	ADC1->SQR1 &= ~  ADC_SQR1_L_Msk;
-	ADC1->SQR1 &= ~ ADC_SQR1_SQ1_Msk;
-	ADC1->SQR1 |=  ADC_SQR1_SQ1_0|ADC_SQR1_SQ1_1|ADC_SQR1_SQ1_2;
+	ADC1->CFGR &= ~ADC_CFGR_RES;   //Configure the resolution as 12 bits 
+	ADC1->CFGR &= ~ADC_CFGR_ALIGN; //Select right alignment
+	ADC1->SQR1 &= ~ADC_SQR1_L_Msk;
+	ADC1->SQR1 &= ~ADC_SQR1_SQ1_Msk;
+	ADC1->SQR1 |= ADC_SQR1_SQ1_0|ADC_SQR1_SQ1_1|ADC_SQR1_SQ1_2;
 	ADC1->DIFSEL &= ~ ADC_DIFSEL_DIFSEL_7;
 	ADC1->SMPR1 |= ADC_SMPR1_SMP7_1;
 	ADC1->CFGR &= ~  ADC_CFGR_CONT;
